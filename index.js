3af945b9ac34be382cebc453a6b182bffb57081d
bandydos <alexandertack@hotmail.be>
2020-01-28 00:27:40 +0100

    Bootstrap works now (express.static in index.js)

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 //server code
+
 const express = require("express");
 const app = express();
 app.listen(3000, () => console.log("Listening on 3000."));
 app.use(express.static("public")); //Folder public.
+app.use(express.static("node_modules/jquery/dist")); //Use jQuery.
+app.use(express.static("node_modules/bootstrap/dist")); //Use bootstrap.
