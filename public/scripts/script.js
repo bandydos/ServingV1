4dde6940b4d8d32de0a91bdb5e95bd1ad3b83d06
KR44I <38465150+AlexaTack@users.noreply.github.com>
2020-01-30 15:29:16 +0100

    final for serving

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -4,12 +4,3 @@ const messageTable = document.getElementById("message-table");
 btnSend.onclick = function(){
     messageIn.value = "";
 }
-if ('geolocation' in navigator){ //If geolocation is available.
-    console.log("works");
-
-    navigator.geolocation.getCurrentPosition(position => {
-        console.log(position.coords);
-    });
-} else {
-    console.log("doesnt work");
-}
